b2b09f19f55055940add90b9d75e9863be1ce51a
Marrietify <marrietify@gmail.com>
2022-10-19 19:04:10 +0000

    Get day of the week

--- TryingMethods.java
+++ TryingMethods.java
@@ -2,4 +2,25 @@ public class TryingMethods {
     public static void main(String[] args) {
         
     }
+    public String weekDay(int dayInteger) {
+        switch (dayInteger) {
+            case 1: 
+                return "monday";
+                case 2: 
+                return "tuesday"; 
+                case 3: 
+                return "wednesday";
+                case 4: 
+                return "thursday";  
+                case 5: 
+                return "friday";
+                case 6: 
+                return "saturday";
+                case 7: 
+                return "sunday";
+        
+            default:
+                return "days of the week not found";
+        }
+    }
 }
